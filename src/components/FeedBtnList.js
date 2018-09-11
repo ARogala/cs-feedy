5fe3efb44319441dfef6acbb9f088845cc9ae258
Andrew Rogala <adrogala87@gmail.com>
2018-09-11 15:46:54 -0400

    build feed button list from array of feeds

--- src/components/FeedBtnList.js
+++ src/components/FeedBtnList.js
@@ -3,12 +3,90 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function FeedBtnList(props) {
-	console.log(props.allFeeds);
 
+	const allFeeds = props.allFeeds;
+
+	//https://stackoverflow.com/questions/4878756/how-to-capitalize-first-letter-of-each-word-like-a-2-word-city
+	function toTitleCase(str) {
+		return str.replace(/\w\S*/g, function(txt){
+		    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+		});
+	}
+	//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+	function groupBy(objectArray, property) {
+		return objectArray.reduce(function (acc, obj) {
+			//remove case sensitivity
+			obj[property] = toTitleCase(obj[property]);
+			//store the category in key
+			let key = obj[property];
+			//if the accumulator object at key is undefined place empty array
+			if (!acc[key]) {
+			  acc[key] = [];
+			}
+			//push objects with the same key into their array
+			//return accumulator
+			acc[key].push(obj);
+			return acc;
+		}, {});
+	}
+
+	//groupedFeeds is an object with arrays of feeds for each category {category1:[{...}], category2:[{...}], ...}
+	const groupedFeeds = groupBy(allFeeds, 'category');
+	console.log(groupedFeeds);
+	const allCategories = Object.keys(groupedFeeds);
+	console.log(allCategories);
+
+	/*
+		for each feed category if the number of feeds is greater than 1
+		build the DOM
+	*/
+	const dropDownUL = [];
+	for(let i = 0; i < allCategories.length; i++ ) {
+		if(groupedFeeds[allCategories[i]].length > 1) {
+			//build the dropDownUL
+			dropDownUL.push(
+				<li key={i}>
+					<span>{allCategories[i]}:</span>
+					<ul aria-label="submenu" className="dropDown">
+						{groupedFeeds[allCategories[i]].map((feed) => {
+							return (
+								<li
+									key={feed.id}
+									role="button"
+									tabIndex="0"
+								>
+									{feed.name + ' - ' + feed.category}
+								</li>
+							);
+						})}
+					</ul>
+				</li>
+			);
+		}
+	}
+
+	//build the DOM for the categories with one feed
+	const singleFeed = [];
+	for(let i = 0; i < allCategories.length; i++) {
+		if(groupedFeeds[allCategories[i]].length === 1) {
+			singleFeed.push(
+				<li
+					className="singleFeed"
+					key={groupedFeeds[allCategories[i]][0].id}
+					role="button"
+					tabIndex="0"
+				>
+					{groupedFeeds[allCategories[i]][0].name + ' - ' + groupedFeeds[allCategories[i]][0].category}
+				</li>
+			);
+		}
+	}
 
 	return (
-		<ul>
-			<li>RSS Feed button/link</li>
+		<ul className="feedBtnList">
+			{dropDownUL}
+			<span>Single Feeds:</span>
+			{singleFeed}
 		</ul>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -9,79 +9,92 @@ let allFeeds = [
 	{
 		name: 'Nasa Image of the Day',
 		category: 'Science',
-		url:  'http://www.nasa.gov/rss/dyn/image_of_the_day.rss'
+		url:  'http://www.nasa.gov/rss/dyn/image_of_the_day.rss',
+		id: 0
 	},
 
 	{
 		name: 'HTML5 Rocks',
 		category: 'WeB Development',
-		url: 'http://feeds.feedburner.com/html5rocks'
+		url: 'http://feeds.feedburner.com/html5rocks',
+		id: 1
 	},
 
 	{
 		name: 'David Kleinert Photography',
 		category: 'Photography',
-		url: 'http://www.davidkphotography.com/index.php?x=atom'
+		url: 'http://www.davidkphotography.com/index.php?x=atom',
+		id: 2
 	},
 
 	{
 		name: 'Reddit Video',
 		category: 'Reddit',
-		url: 'http://www.reddit.com/r/videos/.rss'
+		url: 'http://www.reddit.com/r/videos/.rss',
+		id: 3
 	},
 
 	{
 		name: 'Outdoor Photographer',
 		category: 'Photography',
-		url: 'https://www.outdoorphotographer.com/feed/'
+		url: 'https://www.outdoorphotographer.com/feed/',
+		id: 4
 	},
 
 	{
 		name: 'Shutterstock',
 		category: 'Photography',
-		url: 'https://www.petapixel.com/feed'
+		url: 'https://www.petapixel.com/feed',
+		id: 5
 	},
 
 	{
 		name: 'Smithsonian Mag',
 		category: 'SmithsOniaN',
-		url: 'https://www.smithsonianmag.com/rss/multimedia/'
+		url: 'https://www.smithsonianmag.com/rss/multimedia/',
+		id: 6
 	},
 
 	{
 		name: 'Smithsonian Food',
 		category: 'Smithsonian',
-		url: 'https://www.smithsonianmag.com/rss/food/'
+		url: 'https://www.smithsonianmag.com/rss/food/',
+		id: 7
 	},
 
 	{
 		name: 'Wired',
 		category: 'Technology',
-		url: 'http://www.wired.com/feed'
+		url: 'http://www.wired.com/feed',
+		id: 8
 	},
 
 	{
 		name: 'CSS-Tricks',
 		category: 'Web development',
-		url: 'http://css-tricks.com/feed/'
+		url: 'http://css-tricks.com/feed/',
+		id: 9
 	},
 
 	{
 		name: 'Smithsonian Videos',
 		category: 'Smithsonian',
-		url: 'https://www.smithsonianmag.com/rss/videos/'
+		url: 'https://www.smithsonianmag.com/rss/videos/',
+		id: 10
 	},
 
 	{
 		name: 'Reddit Home',
 		category: 'Reddit',
-		url: 'https://www.reddit.com/.rss'
+		url: 'https://www.reddit.com/.rss',
+		id: 11
 	},
 
 	{
 		name: 'JS Weekly',
 		category: 'web Development',
-		url: 'https://javascriptweekly.com/rss/1a177hd2'
+		url: 'https://javascriptweekly.com/rss/1a177hd2',
+		id: 12
 	}
 ];
 
