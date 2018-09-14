b9c4fbcc93f689029cac01dd3c8a1a0733cf77a9
Andrew Rogala <adrogala87@gmail.com>
2018-09-14 10:04:10 -0400

    build basic delete list

--- src/components/DeleteFeedBtnList.js
+++ src/components/DeleteFeedBtnList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function DeleteFeedBtnList(props) {
-	let allFeeds = props.allFeeds;
+	const allFeeds = props.allFeeds;
 	console.log(allFeeds);
 	//https://stackoverflow.com/questions/4878756/how-to-capitalize-first-letter-of-each-word-like-a-2-word-city
 	function toTitleCase(str) {
@@ -33,10 +33,64 @@ function DeleteFeedBtnList(props) {
 	const allCategories = Object.keys(groupedFeeds);
 	//console.log(allCategories);
 
+	const dropDownUL = [];
+	for(let i = 0; i < allCategories.length; i++ ) {
+		if(groupedFeeds[allCategories[i]].length > 1) {
+			//build the dropDownUL
+			dropDownUL.push(
+				<li key={i}>
+					<span>{allCategories[i]}:</span>
+					<ul aria-label="submenu" className="dropDown">
+						{groupedFeeds[allCategories[i]].map((feed) => {
+							return (
+								<li
+									//onClick={() => handleClick(feed.url)}
+									//onKeyPress={() => handleClick(feed.url)}
+									key={feed.id}
+									id={feed.id}
+									role="button"
+									tabIndex="0"
+								>
+									{feed.name + ' - ' + feed.category}
+								</li>
+							);
+						})}
+					</ul>
+				</li>
+			);
+		}
+	}
 
+	//build the DOM for the categories with one feed
+	const singleFeed = [];
+	for(let i = 0; i < allCategories.length; i++) {
+		if(groupedFeeds[allCategories[i]].length === 1) {
+			singleFeed.push(
+				<li
+					className="singleFeed"
+					//onClick={() => handleClick(groupedFeeds[allCategories[i]][0].url)}
+					//onKeyPress={() => handleClick(groupedFeeds[allCategories[i]][0].url)}
+					key={groupedFeeds[allCategories[i]][0].id}
+					id={groupedFeeds[allCategories[i]][0].id}
+					role="button"
+					tabIndex="0"
+				>
+					{groupedFeeds[allCategories[i]][0].name + ' - ' + groupedFeeds[allCategories[i]][0].category}
+				</li>
+			);
+		}
+	}
 
+	console.log(allFeeds);
+	//set local storage equal to the modified allFeeds array
+	//this adds the Ids to the localStrorage allFeeds
+	localStorage.setItem('allFeeds', JSON.stringify(allFeeds));
 	return (
-		<div>Delete Feeds</div>
+		<ul className="feedBtnList">
+			{dropDownUL}
+			<span>Single Feeds:</span>
+			{singleFeed}
+		</ul>
 	);
 }
 
