7b1ca40c62a365b85f3dcd79538cd80aa2bf7fd4
ARogala <adrogala87@gmail.com>
2018-11-15 20:07:21 -0500

    fix nav css import statement

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import focusWithin from 'focus-within';
 
-import './Navigation.css';
+import './navigation.css';
 
 focusWithin(document);
 
